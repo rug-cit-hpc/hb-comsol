906f1e2990a2020a197955462a2a0661a4a779b1
Cristian A. Marocico <c.a.marocico@rug.nl>
2022-09-22 06:14:32 +0200

    Optimized

--- form.js
+++ form.js
@@ -1,61 +1,44 @@
-'use strict'
+// 'use strict'
 
-/**
- * Fix num cores, allowing blanks to remain
- */
-function fix_num_cores() {
-  let node_type_input = $('#batch_connect_session_context_node_type');
-  let node_type = node_type_input.val();
-  let num_cores_input = $('#num_cores');
+// /**
+//  * Fix num cores, allowing blanks to remain
+//  */
+// function fix_num_cores() {
+//   let node_type_input = $('#batch_connect_session_context_node_type');
+//   let node_type = node_type_input.val();
+//   let num_cores_input = $('#num_cores');
+  
+//   if(num_cores_input.val() === '') {
+//     return;
+//   }
+  
+//   let num_cores = 24;
 
-  if(num_cores_input.val() === '') {
-    return;
-  }
+//   if(node_type === 'Regular') {
+//     num_cores = 28;
+//   } else if(node_type === 'himem') {
+//     num_cores = 48;
+//   } else if(node_type === 'gelifes') {
+//     num_cores = 64;
+//   } 
+  
+  
+//   num_cores_input.attr('max', num_cores);
+//   num_cores_input.attr('min', 1);
+//   num_cores_input.val(1);
+//   console.log(num_cores_input.val());
+// }
 
-  if(node_type === 'hugemem') {
-    set_ppn_owens_hugemem(num_cores_input);
-  } else {
-    set_ppn_owens_regular(num_cores_input);
-  }
-}
+// /**
+//  * Change the maximum number of cores selected.
+//  */
+// function set_node_type_change_handler() {
+//   let node_type_input = $('#batch_connect_session_context_node_type');
+//   node_type_input.change(node_type_input, fix_num_cores);
+// }
 
-/**
- * Sets the PPN limits available for Owens hugemem nodes.
- * 
- * hugemem reservations are always assigned the full node
- *
- * @param      {element}  num_cores_input  The input for num_cores
- */
-function set_ppn_owens_hugemem(num_cores_input) {
-  const NUM_CORES = 48;
-  num_cores_input.attr('max', NUM_CORES);
-  num_cores_input.attr('min', NUM_CORES);
-  num_cores_input.val(NUM_CORES);
-}
-
-/**
- * Sets the PPN limits available for non hugemem Owens nodes.
- *
- * @param      {element}  num_cores_input  The input for num_cores
- */
-function set_ppn_owens_regular(num_cores_input) {
-  const NUM_CORES = 24;
-  num_cores_input.attr('max', NUM_CORES);
-  num_cores_input.attr('min', 1);
-  num_cores_input.val(Math.min(NUM_CORES, num_cores_input.val()));
-}
-
-
-/**
- * Change the maximum number of cores selected.
- */
-function set_node_type_change_handler() {
-  let node_type_input = $('#batch_connect_session_context_node_type');
-  node_type_input.change(node_type_input, fix_num_cores);
-}
-
-$(document).ready(function() {
-  // Set the max value to be what was set in the last session
-  fix_num_cores();
-  set_node_type_change_handler();
-});
+// $(document).ready(function() {
+//   // Set the max value to be what was set in the last session
+//   fix_num_cores();
+//   set_node_type_change_handler();
+// });
